16ff84007d5609c0521d8cb0720d8493c1ce1bac
vickoman <vicmandlagasca@gmail.com>
2022-04-08 00:29:03 -0500

    hide buttons if not have access

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -39,11 +39,17 @@
                                             @endif
                                             </td>
                                             <td>
+                                            @can('user-show')
                                             <a class="btn btn-info" href="{{ route('users.show',$user->id) }}">Show</a>
+                                            @endcan
+                                            @can('user-edit')
                                             <a class="btn btn-primary" href="{{ route('users.edit',$user->id) }}">Edit</a>
+                                            @endcan
+                                            @can('user-delete')
                                             {!! Form::open(['method' => 'DELETE','route' => ['users.destroy', $user->id],'style'=>'display:inline']) !!}
                                                 {!! Form::submit('Delete', ['class' => 'btn btn-danger']) !!}
                                             {!! Form::close() !!}
+                                            @endcan
                                             </td>
                                         </tr>
                                     @endforeach
